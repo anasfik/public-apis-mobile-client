f7b968350157a8ab27b8491f285a312abfe62940
Gwhyyy <ffikhi.aanas@gmail.com>
2022-10-01 16:59:00 +0100

    [ Edit ] saved last changes

--- lib/app/modules/views/categories_view/widgets/bookmark_page_icon.dart
+++ lib/app/modules/views/categories_view/widgets/bookmark_page_icon.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../../../data/models/favoriteApi.dart';
