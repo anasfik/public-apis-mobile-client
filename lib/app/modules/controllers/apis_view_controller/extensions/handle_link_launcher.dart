c740238bef3ece3d31872738bbcaf7611bf736c2
Gwhyyy <ffikhi.aanas@gmail.com>
2022-12-01 22:45:56 +0100

    [ Edit ] refactored handle_link_launcher

--- lib/app/modules/controllers/apis_view_controller/extensions/handle_link_launcher.dart
+++ lib/app/modules/controllers/apis_view_controller/extensions/handle_link_launcher.dart
@@ -6,42 +6,50 @@ import 'package:url_launcher/url_launcher.dart';
 import '../../../../services/crashlytics/crashlytics.dart';
 
 extension HandleLaunchLinkExtension on ApisViewController {
-  /// Launch link method handler
+  /// Launch link method handler.
   Future<void> handleLaunchLink(String link, context) async {
-    // Get parsed link
-    final Uri parsedLink = Uri.parse(
-      link,
-    );
+    final Uri parsedLink = Uri.parse(link);
     final bool canLaunch = await canLaunchUrl(parsedLink);
 
-    // Check if link is valid
     if (canLaunch) {
-      // Then launch
       await tryMultipleLaunchModes(parsedLink);
-
-      analytics.analyticsInstance
-          .logEvent(name: "links_opened_to_users_successfully", parameters: {
-        "link": link,
-      });
+      _logSuccessfullyOpenedApis(link);
       hiveService.incrementNumberOfApisOpenedByUser();
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content:
-              Text("could not open the API link, we are working to fix it"),
-        ),
-      );
-      analytics.analyticsInstance.logEvent(
-        name: "links_not_opened_to_users",
-        parameters: {
-          "link": link,
-        },
-      );
-
-      Crashlytics().crashlyticsInstance.recordError(
-            Exception("Link not opened / valid"),
-            StackTrace.current,
-          );
+      _showSnackbarOfFailureToUser(context);
+      _logUnSuccessfullyOpenedApis(link);
+      _recordErrorWithCrashlytics();
     }
+  }
+
+  void _showSnackbarOfFailureToUser(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("could not open the API link, we are working to fix it"),
+      ),
+    );
+  }
+
+  void _recordErrorWithCrashlytics() {
+    Crashlytics().crashlyticsInstance.recordError(
+          Exception("Link not opened / valid"),
+          StackTrace.current,
+        );
+  }
+
+  void _logUnSuccessfullyOpenedApis(String link) {
+    analytics.analyticsInstance.logEvent(
+      name: "links_not_opened_to_users",
+      parameters: {
+        "link": link,
+      },
+    );
+  }
+
+  void _logSuccessfullyOpenedApis(String link) {
+    analytics.analyticsInstance
+        .logEvent(name: "links_opened_to_users_successfully", parameters: {
+      "link": link,
+    });
   }
 }
