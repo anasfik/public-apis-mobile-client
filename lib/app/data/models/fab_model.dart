f2f84973946ea53885a828099eef4fd1ef4cdb5d
Gwhyyy <ffikhi.aanas@gmail.com>
2022-12-02 02:01:16 +0100

    [ Add ] added implementation of the hasshcode and == ovverides

--- lib/app/data/models/fab_model.dart
+++ lib/app/data/models/fab_model.dart
@@ -5,12 +5,22 @@ typedef FabCallback = void Function();
 class FabData {
   final FabCallback callback;
   final FabDirectionOption direction;
-  
+
   FabData({
     required this.callback,
     required this.direction,
   });
+
+  @override
+  operator ==(covariant FabData other) {
+    return direction.name == other.direction.name;
+  }
+
+  // ignore: member-ordering
+  @override
+  int get hashCode => direction.name.hashCode;
 }
+
 enum FabDirectionOption {
   up,
   down,
