b00dbd66b926b05092c0583cf6b02ac9eb8d5454
Gwhyyy <ffikhi.aanas@gmail.com>
2022-12-07 02:32:46 +0100

    [ Add ] added error and success text that will be shown to user to options

--- lib/app/modules/controllers/category_box_controller/extensions/category_box_options_extension.dart
+++ lib/app/modules/controllers/category_box_controller/extensions/category_box_options_extension.dart
@@ -8,36 +8,46 @@ import 'package:public_apis_desktop_client/app/modules/controllers/category_box_
 import '../category_box_controller.dart';
 
 extension CategoryBoxOptionsMixin on CategoryBoxController {
-  List<CategoryBoxOption> get options => <CategoryBoxOption>[
-        CategoryBoxOption(
-          title: "copy",
-          icon: Icons.copy,
-          onTap: copyCategoryTitle,
-          onErrorText: "cannot copy category, please try again",
-          onSuccessText: "Copied!",
-        ),
-        CategoryBoxOption(
-          title: "add all apis to bookmarks",
-          icon: Icons.bookmark_add,
-          onTap: addAllApisToBookmarks,
-        ),
-        CategoryBoxOption(
-          title: "remove all apis from bookmarks",
-          icon: Icons.bookmark_remove,
-          onTap: removeAllApisFromBookmarks,
-        ),
-        CategoryBoxOption(
-          title: "hide category",
-          icon: Icons.visibility_off,
-          onTap: hideCategory,
-          onErrorText: "cannot hide category, please try again",
-          onSuccessText:
-              "Category hidden successfully, you can show it again from the settings",
-        ),
-        CategoryBoxOption(
-          title: "share",
-          icon: Icons.share,
-          onTap: shareCategory,
-        ),
-      ];
+  List<CategoryBoxOption> get options {
+    final category = CategoryBoxController.currentCategoryData!.title;
+
+    return <CategoryBoxOption>[
+      CategoryBoxOption(
+        title: "copy",
+        icon: Icons.copy,
+        onTap: copyCategoryTitle,
+        onErrorText: "cannot copy category, please try again",
+        onSuccessText: "Copied!",
+      ),
+      CategoryBoxOption(
+        title: "add all apis to bookmarks",
+        icon: Icons.bookmark_add,
+        onTap: addAllApisToBookmarks,
+        onErrorText: "cannot add apis to bookmarks, please try again",
+        onSuccessText: "$category apis added to bookmarks",
+      ),
+      CategoryBoxOption(
+        title: "remove all apis from bookmarks",
+        icon: Icons.bookmark_remove,
+        onTap: removeAllApisFromBookmarks,
+        onErrorText: "cannot remove apis from bookmarks, please try again",
+        onSuccessText: "$category apis removed from bookmarks",
+      ),
+      CategoryBoxOption(
+        title: "hide category",
+        icon: Icons.visibility_off,
+        onTap: hideCategory,
+        onErrorText: "cannot hide category, please try again",
+        onSuccessText:
+            "Category hidden successfully, you can show it again from the settings",
+      ),
+      CategoryBoxOption(
+        title: "share",
+        icon: Icons.share,
+        onTap: shareCategory,
+        onErrorText: "cannot share, please try again",
+        onSuccessText: "Shared!",
+      ),
+    ];
+  }
 }
